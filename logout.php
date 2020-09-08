a1dca5aaf54ad41c585d6eb0f741112ab4d1f888
silkslime <silkslime@yandex.ru>
2020-09-08 21:49:57 +0300

    dev

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
+require('util/functions.php');
 session_start();
+$user = $_SESSION["username"];
+$isSU = $_SESSION["su"];
+$dbconn = pg_connect(getenv("DATABASE_URL"));
+$method = $_SERVER["REQUEST_METHOD"];
+
 if (!$user)
 {
     session_destroy();
