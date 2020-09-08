be555cccb35afeeb49183b16a055e674dfbd1578
silkslime <silkslime@yandex.ru>
2020-09-08 12:40:55 +0300

    dev

--- test.php
+++ test.php
@@ -1,17 +1,3 @@
 <?php=
 echo("<div>AAA</div>");
-session_start();
-echo("<div>BBB</div>");
-$method = $_SERVER["REQUEST_METHOD"];
-echo("<div>AAA</div>");
-$db = parse_url(getenv("DATABASE_URL"));
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-echo("<div>AAA</div>");
 ?>
