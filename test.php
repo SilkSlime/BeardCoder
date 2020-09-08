a53690cd02e483447d9e5387345b29b6e9f810c2
silkslime <silkslime@yandex.ru>
2020-09-08 12:27:29 +0300

    dev

--- test.php
+++ test.php
@@ -1,12 +1,14 @@
 <?php
-echo("<h1>AAA</h1>");
+echo("<div>AAA</div>");
 session_start();
+echo("<div>BBB</div>");
 $user = session("username");
+echo("<div>AAA</div>");
 $method = $_SERVER["REQUEST_METHOD"];
-
+echo("<div>AAA</div>");
 echo($user);
 echo($method);
-
+echo("<div>AAA</div>");
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
@@ -16,8 +18,10 @@ $pdo = new PDO("pgsql:" . sprintf(
     $db["pass"],
     ltrim($db["path"], "/")
 ));
-
+echo("<div>AAA</div>");
 
 $data = $pdo->pgsqlCopyToArray('Users');
+echo("<div>AAA</div>");
 echo(var_dump($data));
+echo("<div>AAA</div>");
 ?>
