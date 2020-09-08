7c83a2d6ec5160e4eafcf1cb5f7af4556fc63fa5
silkslime <silkslime@yandex.ru>
2020-09-08 23:56:50 +0300

    dev2

--- addcode.php
+++ addcode.php
@@ -4,7 +4,7 @@ $dbconn = pg_connect(getenv("DATABASE_URL"));
 $method = $_SERVER["REQUEST_METHOD"];
 
 if ($method == "POST") {
-    echo var_dump($_POST);
+    echo "planned text!";
 }
 
 ?>
---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import time
+import os
+        
+def main():
+    print("Hi")
+    postData = {'provider': "Dominos", 'code': '123123'}
+    response = requests.post('https://beardcoder.herokuapp.com/addcode.php', json = postData)
+    print(response)
+    print(response.text)
+    
+    
+main()
