b3411dbeac622df431beb64f80499ac9ccde477f
silkslime <silkslime@yandex.ru>
2020-09-08 13:13:34 +0300

    dev

---
+++ register.php
@@ -0,0 +1,36 @@
+<form action="signup.php" method="POST" id="form">
+    <div class="form-row">
+        <div class="col-md-4 mb-3">
+            <label>Name</label>
+            <input type="text" class="form-control" name="name" placeholder="John" required>
+        </div>
+        <div class="col-md-4 mb-3">
+            <label>Surname</label>
+            <input type="text" class="form-control" name="surname" placeholder="Wick" required>
+        </div>
+        <div class="col-md-4 mb-3">
+            <label>Surname</label>
+            <input type="text" class="form-control" name="group" placeholder="IU4-12B" required>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-md-6 mb-3">
+            <label>Email</label>
+            <input type="email" class="form-control" name="email" placeholder="example@mail.com" required>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label>Username</label>
+            <input type="text" class="form-control" name="username" placeholder="John1337" required>
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col-md-6 mb-3">
+            <label>Password</label>
+            <input type="password" class="form-control" name="pass" placeholder="Rassword" required>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label>Repeat password</label>
+            <input type="password" class="form-control" name="pass2" placeholder="Repeat password" required>
+        </div>
+    </div>
+</form>
