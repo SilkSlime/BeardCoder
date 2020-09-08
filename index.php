2fa8e088d66b03b822c5e487c58a1ec8490ca5f6
silkslime <silkslime@yandex.ru>
2020-09-08 17:22:53 +0300

    dev

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require('utility/functions.php');
+require('./utility/functions.php');
 session_start();
 $user = $_SESSION["username"];
 $isAdmin = $_SESSION["su"];
@@ -38,7 +38,6 @@ if ($method == "POST") {
     <title>View'n'Control</title>
     <link rel="icon" type="image/png" href="../ico/mustache.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-require('utility/functions.php');
+require('./utility/functions.php');
 session_start();
 $user = $_SESSION["username"];
 $isAdmin = $_SESSION["isAdmin"];
