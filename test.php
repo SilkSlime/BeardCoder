2bcbd8b3bd43efc63b21869d934f4b1a16f1cc5e
silkslime <silkslime@yandex.ru>
2020-09-08 12:50:30 +0300

    dev

--- test.php
+++ test.php
@@ -1,14 +1,4 @@
 <?php
 session_start();
 echo(var_dump($_SESSION);
-
-$db = parse_url(getenv("DATABASE_URL"));
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
 ?>
