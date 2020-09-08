db21b1ed1159e578fab4a6d32e49c54705a20b99
silkslime <silkslime@yandex.ru>
2020-09-08 12:45:53 +0300

    dev

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
 <?php
-echo("<div>AAA</div>");
 session_start();
-echo("<div>BBB</div>");
-echo("<div>AAA</div>");
-echo(var_dump($_SESSION));
+echo(var_dump($_SESSION);
+$method = $_SERVER["REQUEST_METHOD"];
+
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
 ?>
