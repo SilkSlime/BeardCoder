2c1983dc5e18dcc3447dd0640d5dfc46f00136b0
silkslime <silkslime@yandex.ru>
2020-09-09 00:11:13 +0300

    dev2

--- addcode.php
+++ addcode.php
@@ -4,8 +4,28 @@ $dbconn = pg_connect(getenv("DATABASE_URL"));
 $method = $_SERVER["REQUEST_METHOD"];
 
 if ($method == "POST") {
-    echo "Planned text!";
-    echo var_dump($_POST);
+    $username = pg_escape_string($_POST["username"]);
+    $password = $_POST["password"];
+
+    $code = pg_escape_string($_POST["code"]);
+    $shop = pg_escape_string($_POST["shop"]);
+    $owner = $username;
+    $badge = pg_escape_string($_POST["badge"]);
+    $status = pg_escape_string($_POST["status"]);
+    $extra = pg_escape_string($_POST["extra"]);
+    
+    $query = "SELECT * FROM users WHERE username='$username';";
+    $result = pg_query($query));
+    $line = pg_fetch_assoc($result);
+    if (password_verify($password, $line['passwordhash'])) {
+        $query = "INSERT INTO codes (code, shop, `owner`, badge, `status`, extra) VALUES ($code, $shop, $owner, $badge, $status, $extra);";
+        $result = pg_query($query));
+        echo $result;
+    } else {
+        echo 'Wrong password/username!';
+    }
+} else {
+    redirect('/');
 }
 
 ?>
